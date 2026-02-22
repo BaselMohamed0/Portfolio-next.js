14d5047ac443236908cd8f76515d9e2a7f67308d
Basel Mohamed <baselmohamed0@outlook.com>
2026-02-22 19:27:06 +0200

    update 22-2-2026

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,30 @@
 import "./globals.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "Basel Mohamed | Frontend Engineer",
   description: "Modern high-performance web experiences built with Next.js",
+  other: {
+    author: "Basel Mohamed",
+    copyright: "© 2026 Basel Mohamed. All Rights Reserved",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-black text-zinc-100 antialiased scroll-smooth">
+        {/* google analytics */}  
+        <GoogleAnalytics gaId="G-EL25SCDC7C" />
+       
         {children}
+
+
+        {/* Vercel analytics */}
+        <Analytics/>
       </body>
     </html>
   );
